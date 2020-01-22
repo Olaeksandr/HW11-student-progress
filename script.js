88ecc23dc7b183e98df361a80ef1d39ae99f6f44
Oleksandr Masiuk <ommasiuk@gmail.com>
2020-01-22 18:07:57 +0200

    first version

--- script.js
+++ script.js
@@ -1,20 +1,58 @@
-'use strict';
-const calculator = createCalculator(10); 
+"use strict";
+let lengthAllArray = null;
+let sumAllArray = null;
+let sumCurrentArray;
+const students = [
+    new Student('Oleksandr', [10, 9, 9, 8, 8, 8, 9, 9, 10, 8]),
+    new Student('Dmitriy', [10, 0, 8, 0, 3, 4, 5]),
+    new Student('Sergio', [2, 5, 6, 7]),
 
-function createCalculator(number) {
-    return {
-        add: value => (number + value),
-        sub: value => (number - value),
-        divide: value => (number / value),
-        mult: value => (number * value),
-        set: value => (number = value)
+];
+
+function Student(name, marks) {
+    this.name = name;
+    this.marks = marks;
+
+    this.maxMark = function () {
+        return Math.max.apply(null, marks);
+        // marks.sort(compareNumbers);//second variant
+        // return marks[marks.length-1];//second variant
+    }
+    this.minMark = function () {
+        return Math.min.apply(null, marks);
+        // marks.sort(compareNumbers);//second variant
+        // return marks[0];//second variant
+    }
+    this.averageMark = function () {
+        const sumOfAllMarks = marks.reduce((acc, mark) => (acc + mark));
+        return sumOfAllMarks / marks.length;
+    }
+    this.getName = function () {
+        return name;
     }
 }
 
-console.log(`check 55 = ${calculator.add(45)};`);// возвращает 55 
-console.log(`check -35 = ${calculator.sub(45)};`);// возвращает -35 
-console.log(`check 2 = ${calculator.divide(5)};`); //возвращает 2 
-console.log(`check 50 = ${calculator.mult(5)};`);// возвращает 50 
-console.log(`check 100 = ${calculator.set(100)};`);// устанавливает базовое значение в 100 
-console.log(`check 500 = ${calculator.mult(5)};`);// возвращает 50 
+// function compareNumbers(a, b) {//second variant
+//   return  a-b;//second variant
+// }//second variant
 
+function averageMark() {
+    for (let index of students) {
+        sumCurrentArray = index.marks.reduce((acc, mark) => (acc + mark));
+        lengthAllArray = lengthAllArray + index.marks.length
+        sumAllArray += sumCurrentArray;
+        }
+    console.log(`Средний бал всех студентов: ${sumAllArray/lengthAllArray}`);
+}
+
+for(let i=0; i<students.length; i++) {
+    console.log(`Имя студента: ${students[i].name}`);
+    console.log(`Имя студента вывод методом: ${students[i].getName()}`);
+    console.log(`Оценки студента ${students[i].name}: ${students[i].marks}`);
+    console.log(`Наибольшая оценка студента ${students[i].name}: ${students[i].maxMark()}`);
+    console.log(`Наименьшая оценка студента ${students[i].name}: ${students[i].minMark()}`);
+    console.log(`Средняя оценка студента ${students[i].name}: ${students[i].averageMark()}`);
+    console.log('*****************************'); 
+}
+
+averageMark();
